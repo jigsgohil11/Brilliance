3d64fa00c6991d13cf60d35d02698196fba51e4c
Neha Mordia <mordia.neha@gmail.com>
2020-02-04 12:31:01 +0530

    Commit CIS design

---
+++ Brilliance/Controllers/AdmCisController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Brilliance.Controllers
+{
+    public class AdmCisController : Controller
+    {
+        //private IAdmCrtDataProvider _iadmcrtDataProvider;
+
+        // GET: AdmCis
+        public ActionResult CISadmin()
+        {
+            //_iadmcrtDataProvider = new AdmCrtDataProvider();
+            //var response = new ServiceResponse();
+            //response = _iadmcrtDataProvider.GetCrtSetup();
+            return View();
+        }
+    }
+}
